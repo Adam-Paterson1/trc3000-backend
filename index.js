2f17e5f3c6ea0ef367c370e46e6bfc3379027997
Adam-Paterson1 <adam.paterson03@gmail.com>
2018-08-02 14:45:35 +1000

    add sockets

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+server.listen(3000);
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
