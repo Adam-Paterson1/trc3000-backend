8299399706f4ce03554cd26c53624edcd1c6c8a5
Adam-Paterson1 <adam.paterson03@gmail.com>
2018-08-05 06:48:36 +0000

    charting and logging added

---
+++ controller.js
@@ -0,0 +1,20 @@
+
+Controller = {
+  target: 0,
+  error: 0,
+  prevVal: 0,
+  kp: 0,
+  kd: 0,
+  ki: 0,
+  run (currVal) {
+    let newErr = this.target - currVal;
+    let dir = currVal - this.prevVal;
+    this.prevVal = currVal;
+    return newErr * this.kp + dir * this.kd;
+  }
+
+}
+
+
+
+module.exports =  Controller
--- index.js
+++ index.js
@@ -1,19 +1,63 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const controller = require('./controller');
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, '../trc3000-frontend/build')))
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 io.on('connection', (client) => {
+  let timerInterval, tiltInterval;
   client.on('subscribeToTimer', (interval) => {
     console.log('client is subscribing to timer with interval ', interval);
-    setInterval(() => {
+    timerInterval = setInterval(() => {
       client.emit('timer', new Date());
     }, interval);
   });
+  client.on('subscribeToTilt', () => {
+    console.log('client is subscribing to tilt with interval 30fps');
+    let i = 1;
+    tiltInterval = setInterval(() => {
+      client.emit('tilt', controller.run(Math.sin(i * Math.PI/ 500)))
+      i++;
+    }, 40)
+  });
+  client.on('disconnect', () => {
+    console.log('clearing intervals')
+    clearInterval(timerInterval);
+    clearInterval(tiltInterval);
+  })
+  client.on('setTarget', (target) => {
+    console.log('setting target', target)
+    controller.target = target
+    console.log('new target', controller.target)
+    client.emit('target', controller.target)
+  })
+  client.on('getTarget', () => {
+    client.emit('target', controller.target)
+  })
+  client.on('setGains', (gains) => {
+    console.log('gains', gains);
+    controller.kp = Number(gains.kp);
+    controller.ki = Number(gains.ki);
+    controller.kd = Number(gains.kd);
+    client.emit('gains', {kp: controller.kp, ki: controller.ki, kd: controller.kd})
+
+  })
+  client.on('getGains', () => {
+    client.emit('gains', {kp: controller.kp, ki: controller.ki, kd: controller.kd})
+  })
 });
-server.listen(3000);
+server.listen(5000);
+
+global.log = function(value) {
+  io.emit('log', value);
+}
+setInterval(() => {
+  log('howdy')
+}, 300)
+
+
 
 // app.listen(3000, () => console.log('Example app listening on port 3000!'))
